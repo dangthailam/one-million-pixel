52e1c9626498f620e6fe37b6a951ed52b6539a35
dangthailam <lamtp1989@gmail.com>
2022-08-07 10:14:28 +0200

    display border pixel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'one-million-pixel';
+  hidden = false;
+  xPos = 0;
+  yPos = 0;
+  
+  pixelSize = 10;
+  edge = 100;
+  pixelArray = Array(this.edge * this.edge);
+
+  ngOnInit() {
+    for (let x = 0; x < this.edge; x++) {
+      for(let y = 0; y < this.edge; y++) {
+        this.pixelArray[x * this.edge + y] = `${y * this.pixelSize}, ${x * this.pixelSize}, ${y * this.pixelSize + this.pixelSize}, ${x * this.pixelSize + this.pixelSize}`;
+      }
+    }
+  }
+
+  displayDiv(element: string) {
+    console.log(element);
+  }
 }
+
+/*
+x1, y1, x2, y2
+0 0 10 10
+10 0 20 10
+*/
