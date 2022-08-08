24662e28c7a1ba5b7f12903ac4bd13a4cd1c66e1
dangthailam <lamtp1989@gmail.com>
2022-08-08 23:42:07 +0200

    implment canvas solution (map area render is too slow)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,13 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, ViewChild} from '@angular/core';
+
+
+export class Square {
+  constructor(private ctx: CanvasRenderingContext2D) {}
+
+  draw(x: number, y: number, z: number) {
+    this.ctx.fillRect(z * x, z * y, z, z);
+  }
+}
 
 @Component({
   selector: 'app-root',
@@ -7,29 +16,64 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'one-million-pixel';
-  hidden = false;
-  xPos = 0;
-  yPos = 0;
-  
-  pixelSize = 10;
-  edge = 100;
-  pixelArray = Array(this.edge * this.edge);
+  private firstX = 0;
+  private firstY = 0;
+  private canvasBound!: DOMRect;
 
-  ngOnInit() {
-    for (let x = 0; x < this.edge; x++) {
-      for(let y = 0; y < this.edge; y++) {
-        this.pixelArray[x * this.edge + y] = `${y * this.pixelSize}, ${x * this.pixelSize}, ${y * this.pixelSize + this.pixelSize}, ${x * this.pixelSize + this.pixelSize}`;
-      }
-    }
+
+  @ViewChild('canvas', { static: true })
+  canvas!: ElementRef<HTMLCanvasElement>;
+
+  private ctx!: CanvasRenderingContext2D;
+
+  ngOnInit(): void {
+    this.canvasBound = this.canvas.nativeElement.getBoundingClientRect();
+    this.ctx = this.canvas.nativeElement.getContext('2d')!;
+
+
+    this.ctx.fillStyle = 'black';
+    this.ctx.fillRect(0, 0, 1000, 1000);
+
+
   }
 
-  displayDiv(element: string) {
-    console.log(element);
+  beginDrawing(event: MouseEvent) {
+    console.log(event);
+    this.firstX = event.clientX - this.canvasBound.left;
+    this.firstY = event.clientY - this.canvasBound.top;
+
   }
+
+  draw(event: MouseEvent) {
+    this.ctx.fillStyle = 'red';
+    const x = event.clientX - this.canvasBound.left;
+    const y = event.clientY - this.canvasBound.top;
+    console.log(x, y);
+    this.ctx.fillRect(this.firstX, this.firstY, x - this.firstX, y - this.firstY);
+  }
+
+  // hidden = true;
+  // xPos = 0;
+  // yPos = 0;
+  //
+  // pixelSize = 10;
+  // edge = 100;
+  // pixelArray = Array(this.edge * this.edge);
+  //
+  // ngOnInit() {
+  //   for (let x = 0; x < this.edge; x++) {
+  //     for(let y = 0; y < this.edge; y++) {
+  //       this.pixelArray[x * this.edge + y] = {
+  //         value: [y * this.pixelSize, x * this.pixelSize, y * this.pixelSize + this.pixelSize, x * this.pixelSize + this.pixelSize],
+  //         coords: `${y * this.pixelSize}, ${x * this.pixelSize}, ${y * this.pixelSize + this.pixelSize}, ${x * this.pixelSize + this.pixelSize}`
+  //       };
+  //     }
+  //   }
+  // }
+  //
+  // displayDiv(element: any) {
+  //   this.hidden = false;
+  //   this.xPos = element.value[0];
+  //   this.yPos = element.value[1];
+  // }
 }
-
-/*
-x1, y1, x2, y2
-0 0 10 10
-10 0 20 10
-*/
